84c006282a7a7033bb6478a4049b9190e1deb073
sokirko <alexey.sokirko@gmail.com>
2020-02-17 12:45:54 +0300

    add arduino source file

---
+++ arduino/vanino.ino
@@ -0,0 +1,20 @@
+const int ger_count = 4;
+const int ger_pins[ger_count] = {3,9,6,11};
+int save_ger_no = -1;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+   for (int ger_no = 0 ; ger_no < ger_count; ++ger_no)  {
+     if (digitalRead(ger_pins[ger_no])) {
+        if (save_ger_no != ger_no) {
+          Serial.print("switch"); 
+          Serial.println(ger_no); 
+          save_ger_no = ger_no;
+        }
+     };
+   }
+   delay(100); 
+}
