84fa91d6305cd8e9994b723e53f2fa1b626248ba
Alexey Sokirko <sokirko@yandex.ru>
2020-06-02 11:38:24 +0300

    use mouse click to start game

--- python/sorter/sorter.py
+++ python/sorter/sorter.py
@@ -2,27 +2,21 @@
 import sys
 import logging
 import tkinter as tk
-from functools import partial
 import argparse
 import random
 import math
 from PIL import Image, ImageTk
 import time
-from pygame import mixer
 
-
-def myquit(root):
-    root.destroy()
-    sys.exit(0)
+import vlc
 
 
 def play_file(filename):
-    mixer.init()
-    mixer.music.load(os.path.join(os.path.dirname(__file__), "audio", filename))
-    mixer.music.play()
-    while mixer.get_busy():
+    player = vlc.MediaPlayer(os.path.join(os.path.dirname(__file__), "audio", filename))
+    player.play()
+    time.sleep(1)
+    while player.is_playing():
         time.sleep(1)
-
 
 
 def pentagram_points(center_x, center_y, r):
@@ -49,23 +43,68 @@ def pentagram_points(center_x, center_y, r):
     ]
 
 
+class TCollection:
+    basic = 1
+    vehicles = 2
+    geometric = 3
+    russian_abc = 4
+    birds = 5
+    all = 6
+
+    @staticmethod
+    def get_collection_name(id):
+        if id == TCollection.basic:
+            return "basic"
+        elif id == TCollection.vehicles:
+            return "vehicles"
+        elif id == TCollection.geometric:
+            return "geometric"
+        elif id == TCollection.russian_abc:
+            return "russian_abc"
+        elif id == TCollection.birds:
+            return "birds"
+        elif id == TCollection.all:
+            return "all"
+
+
 class TSpriteForm:
-    def __init__(self, form=None, color=None, jpg=None):
+    sprite_width = 200
+    sprite_height = 200
+    font_size = 90
+
+    def __init__(self, collection_id, form=None, color=None, image_path=None, char=None):
+        self.collection_id = collection_id
         self.form = form
         self.color = color
-        self.jpg = jpg
+        self.image_path = image_path
+        self.char = char
         self.image = None
-        if jpg is not None:
-            self.image = ImageTk.PhotoImage(Image.open(jpg))
+        if image_path is not None:
+            image = Image.open(image_path)
+            image = image.resize((TSpriteForm.sprite_width, TSpriteForm.sprite_height), Image.ANTIALIAS)
+            self.image = ImageTk.PhotoImage(image)
 
     def is_equal(self, other):
-        return other.form == self.form and other.color == self.color and other.jpg == self.jpg
+        return other.form == self.form and \
+               other.color == self.color and \
+               other.image_path == self.image_path and \
+               other.char == self.char
+
+    def play_success(self):
+        specific_mp3 = os.path.join("audio", str(self)+".mp3")
+        if os.path.exists(specific_mp3):
+            play_file(os.path.basename(specific_mp3))
+        else:
+            play_file("success.mp3")
 
     def __str__(self):
-        if self.jpg is not None:
-            return "TSpriteForm: jpg={}".format(self.jpg)
+        if self.image_path is not None:
+            main_name, _ = os.path.splitext(os.path.basename(self.image_path))
+            return "{}.{}".format(TCollection.get_collection_name(self.collection_id), main_name)
+        elif self.char is not None:
+            return "char.{}".format(self.char)
         else:
-            return "TSpriteForm:{} {}".format(self.color, self.form)
+            return "geometric.{}.{}".format(self.color, self.form)
 
 
 class TPoint:
@@ -82,81 +121,81 @@ class TSprite:
         self.id = None
 
     def draw_sprite(self):
-        color = self.sprite_form.color
         self.hide_sprite()
 
-        if self.sprite_form.form == "rectanglbe":
+        if self.sprite_form.form == "rectangle":
             self.id = self.frame.canvas.create_rectangle(
                 self.point.x, self.point.y,
-                self.point.x + self.frame.sprite_width, self.point.y + self.frame.sprite_height,
-                outline=color, fill=color)
+                self.point.x + TSpriteForm.sprite_width, self.point.y + TSpriteForm.sprite_height,
+                outline=self.sprite_form.color, fill=self.sprite_form.color)
         elif self.sprite_form.form == "triangle":
             self.id = self.frame.canvas.create_polygon(
                 self.point.x, self.point.y,
-                self.point.x + self.frame.sprite_width,   self.point.y,
-                self.point.x + self.frame.sprite_width / 2, self.point.y + self.frame.sprite_height,
+                self.point.x + TSpriteForm.sprite_width,   self.point.y,
+                self.point.x + TSpriteForm.sprite_width / 2, self.point.y + TSpriteForm.sprite_height,
                 self.point.x, self.point.y,
-                outline=color, fill=color)
+                outline=self.sprite_form.color, fill=self.sprite_form.color)
         elif self.sprite_form.form == "pentagram":
-            points = pentagram_points(self.point.x + self.frame.sprite_width/2,
-                                      self.point.y + self.frame.sprite_height/2,
-                                      self.frame.sprite_width/2)
-            self.id = self.frame.canvas.create_polygon(points, outline=color, fill=color)
+            points = pentagram_points(self.point.x + TSpriteForm.sprite_width/2,
+                                      self.point.y + TSpriteForm.sprite_height/2,
+                                      TSpriteForm.sprite_width/2)
+            self.id = self.frame.canvas.create_polygon(points, outline=self.sprite_form.color, fill=self.sprite_form.color)
         elif self.sprite_form.form == "circle":
             self.id = self.frame.canvas.create_oval(
                 self.point.x, self.point.y,
-                self.point.x + self.frame.sprite_width, self.point.y + self.frame.sprite_height,
-                outline=color, fill=color)
-        elif self.sprite_form.jpg is not None:
-            self.id = self.frame.canvas.create_image(self.point.x + self.frame.sprite_width/2,
-                                           self.point.y + self.frame.sprite_height/2,
+                self.point.x + TSpriteForm.sprite_width, self.point.y + TSpriteForm.sprite_height,
+                outline=self.sprite_form.color, fill=self.sprite_form.color)
+        elif self.sprite_form.image_path is not None:
+            self.id = self.frame.canvas.create_image(self.point.x + TSpriteForm.sprite_width/2,
+                                           self.point.y + TSpriteForm.sprite_height/2,
                                            image=self.sprite_form.image)
+        elif self.sprite_form.char is not None:
+            self.id = self.frame.canvas.create_text(self.point.x + TSpriteForm.sprite_width/2,
+                                                    self.point.y + TSpriteForm.sprite_height/2,
+                                                    text=self.sprite_form.char,
+                                                    justify=tk.CENTER, font="Verdana {}".format(TSpriteForm.font_size))
 
     def hide_sprite(self):
         if self.id is not None:
             self.frame.canvas.delete(self.id)
 
     def matches(self, sprite):
-        if abs(self.point.x - sprite.point.x) < 30 and abs(self.point.y - sprite.point.y) < 30:
+        if abs(self.point.x - sprite.point.x) < 50 and abs(self.point.y - sprite.point.y) < 50:
             return self.sprite_form.is_equal(sprite.sprite_form)
         return None
 
 
-def read_folder(folder):
+def read_folder(collection_id):
     sprites = list()
+    folder = TCollection.get_collection_name(collection_id)
     for fpath in os.listdir(os.path.join(os.path.dirname(__file__), folder)):
-        sprites.append(TSpriteForm(jpg=os.path.join(folder, fpath)) )
+        sprites.append(TSpriteForm(collection_id, image_path=os.path.join(folder, fpath)) )
     return sprites
 
 
-class TCollection:
-    basic = 1
-    vehicle = 2
-    geometric = 3
-    all = 4
-
-
 class TApplication(tk.Frame):
-    def __init__(self, logger, master, goals_count):
+    def __init__(self, args, logger, master):
         super().__init__(master)
+        self.args = args
+        self.goals_count = args.goals_count
         self.logger = logger
         self.redrawing = False
         self.master = master
+        self.set_main_window_geometry()
         self.pack(expand=True, fill=tk.BOTH)
         self.canvas = tk.Canvas(self, bg="white")
         self.goal_up_margin = 10
         self.goal_horizontal_margin = 10
-        self.sprite_width = 100
-        self.sprite_height = 100
         self.collection_id = tk.IntVar(value=TCollection.basic)
         self.bottom_command_line_height = 20
         self.canvas_height = self.master.winfo_height() - self.bottom_command_line_height
-        self.basic_forms = read_folder("basic")
-        self.vehicles_forms = read_folder("vehicles")
-        self.geometric_forms = [TSpriteForm(form=f, color=c) for c in ["blue", "red", "yellow", "black"] \
+        self.basic_forms = read_folder(TCollection.basic)
+        self.vehicles_forms = read_folder(TCollection.vehicles)
+        self.geometric_forms = [TSpriteForm(TCollection.geometric, form=f, color=c) for c in ["blue", "red", "yellow", "black"] \
                                     for f in ["rectangle", "triangle", "circle", "pentagram"] ]
-
-        self.goals_count = goals_count
+        self.russian_abc = [TSpriteForm(TCollection.russian_abc, char=a) for a in "АЕИЯЮЭЁАЕИЯЮЭЁАБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЭЮЯ"]
+                                                        # three times vowels
+        self.bird = read_folder(TCollection.birds)
         self.goals_count_str = tk.StringVar()
         self.goals_count_str.set(str(self.goals_count))
         self.goals = None
@@ -169,8 +208,31 @@ class TApplication(tk.Frame):
         master.bind("<Configure>", self.resize)
         self.master.bind('<Control-n>', self.init_new_game)
         self.master.bind('<Control-N>', self.init_new_game)
-
+        self.master.bind("<Button-1>", self.init_new_game)
+        self.master.bind('<Control-x>', self.quit_command)
+        self.master.bind('<Control-X>', self.quit_command)
+        self.master.bind('<Control-c>', self.stop_game)
+        self.master.bind('<Control-C>', self.stop_game)
+        self.master.bind('<Control-b>', self.bind_mouse)
         self.moving_step = 5
+        master.wm_protocol("WM_DELETE_WINDOW", self.quit_command )
+        self.canvas.update()
+        start_x = (self.canvas.winfo_width() - TSpriteForm.sprite_width) / 2
+        start_y = self.canvas.winfo_height() - TSpriteForm.sprite_height - 2
+        self.start_point = TPoint(start_x, start_y)
+
+    def set_main_window_geometry(self):
+        if self.args.fullscreen:
+            self.master.attributes('-fullscreen', True)
+            TSpriteForm.sprite_width = 300
+            TSpriteForm.sprite_height = 300
+            TSpriteForm.font_size = 180
+        else:
+            TSpriteForm.sprite_width = 100
+            TSpriteForm.sprite_height = 100
+            TSpriteForm.font_size = 45
+            self.master.geometry("600x500+100+100")
+        self.master.update()
 
     def resize(self, event):
         if event.widget == self:
@@ -181,34 +243,37 @@ class TApplication(tk.Frame):
         for g in self.goals:
             m = self.sprite.matches(g)
             if m is not None:
+                self.unbind_mouse_move()
+                self.move_mouse_to_start_position()
                 if m:
                     self.canvas.configure(bg="green")
-                    play_file("success.mp3")
+                    self.canvas.update()
+                    self.sprite.sprite_form.play_success()
                 else:
                     self.canvas.configure(bg="red")
+                    self.canvas.update()
                     play_file("fail.mp3")
-                self.canvas.update()
-                self.unbind_mouse_move()
-                time.sleep(2)
-                self.init_new_game()
+                #self.init_new_game()
                 return
 
-    def redraw_canvas(self, x, y):
+    def redraw_canvas(self, sprite_x, sprite_y):
         if self.sprite.point is None:
-            return
-        if x < 0 or y < 0:
-            return
-        if x > self.canvas.winfo_width() - self.sprite_width:
-            return
-        if y > self.canvas.winfo_height() - self.sprite_height:
             return
         if self.redrawing:
             return
+        if sprite_x <= 0:
+            sprite_x = 1
+        if sprite_y <= 0:
+            sprite_y = 1
+        if sprite_x > self.canvas.winfo_width() - TSpriteForm.sprite_width:
+            sprite_x = self.canvas.winfo_width() - TSpriteForm.sprite_width
+        if sprite_y > self.canvas.winfo_height() - TSpriteForm.sprite_height:
+            sprite_y = self.canvas.winfo_height() - TSpriteForm.sprite_height
         self.redrawing = True
         try:
             self.sprite.hide_sprite()
-            self.sprite.point.x = x
-            self.sprite.point.y = y
+            self.sprite.point.x = sprite_x
+            self.sprite.point.y = sprite_y
             for g in self.goals:
                 g.draw_sprite()
             self.sprite.draw_sprite()
@@ -235,57 +300,88 @@ class TApplication(tk.Frame):
     def down_key(self, event):
         self.redraw_canvas_delta(0, +self.moving_step)
 
-    def motion_handler(self, event):
-        self.redraw_canvas(
-            event.x - self.sprite_width/2,
-            event.y - self.sprite_height/2)
+    def move_mouse(self, x, y):
+        sys.stderr.write("move_mouse {},{}\n".format(x, y))
+        self.event_generate('<Motion>', warp=True, x=x, y=y, when="now")
 
-    def bind_mouse_move(self):
-        self.master.bind('<Motion>', self.motion_handler)
+    def mouse_motion_handler(self, event):
+        if self.args.fullscreen:
+            if event.x > self.canvas.winfo_width() - TSpriteForm.sprite_width:
+                self.move_mouse(self.canvas.winfo_width() - TSpriteForm.sprite_width, event.y)
+                return
+            if event.y > self.canvas.winfo_height() - TSpriteForm.sprite_height:
+                self.move_mouse(event.x, self.canvas.winfo_height() - TSpriteForm.sprite_height)
+                return
+        if event.widget == self.canvas:
+            sys.stderr.write("{},{}, {}\n".format(event.x, event.y, event))
+            self.redraw_canvas(
+                    event.x,
+                    event.y)
+
+    def bind_mouse(self, event):
+        self.master.bind('<Motion>', self.mouse_motion_handler)
+
+    def generate_bind_mouse_event(self):
+        self.event_generate('<Control-b>')
 
     def unbind_mouse_move(self):
         self.master.unbind('<Motion>')
+
+    def move_mouse_to_start_position(self):
+        #sys.stderr.write("move_mouse_to_start_position\n".format())
+        self.move_mouse(self.start_point.x, self.start_point.y)
 
     def get_forms(self):
         c_id = self.collection_id.get()
         if c_id == TCollection.basic:
             return self.basic_forms
-        elif c_id == TCollection.vehicle:
+        elif c_id == TCollection.vehicles:
             return self.vehicles_forms
         elif c_id == TCollection.geometric:
             return self.geometric_forms
+        elif c_id == TCollection.russian_abc:
+            return self.russian_abc
+        elif c_id == TCollection.birds:
+            return self.bird
         else:
-            return self.basic_forms + self.vehicles_forms + self.geometric_forms
+            return self.basic_forms + self.vehicles_forms + self.geometric_forms + self.russian_abc + self.bird
 
-    def init_new_game(self,event=None):
+
+    def init_new_game(self, event=None):
         self.canvas.delete("all")
         self.canvas.configure(bg="white")
         self.goals_count = int(self.goals_count_str.get())
         self.goals = [TSprite(self) for i in range(self.goals_count)]
 
-        start_x = (self.canvas.winfo_width() - self.sprite_width) / 2
-        start_y = self.canvas.winfo_height() - self.sprite_height - 2
-        self.sprite.point = TPoint(start_x, start_y)
-        self.event_generate('<Motion>', warp=True, x=start_x+self.sprite_width/2, y=start_y+self.sprite_height/2)
-        self.bind_mouse_move()
+        self.sprite.point = TPoint(self.start_point.x, self.start_point.y)
         all_forms = self.get_forms()
         forms = random.sample(all_forms, self.goals_count)
         for f in forms:
             self.logger.debug(str(f))
-        sum_goals_width = self.sprite_width * self.goals_count
+        sum_goals_width = TSpriteForm.sprite_width * self.goals_count
         gap_width = (self.canvas.winfo_width() - 2 * self.goal_horizontal_margin - sum_goals_width)/(self.goals_count -1)
         x = self.goal_horizontal_margin
         for f, g in zip(forms, self.goals):
             g.sprite_form = f
             g.point = TPoint(x, self.goal_up_margin)
-            x += self.sprite_width + gap_width
+            x += TSpriteForm.sprite_width + gap_width
             g.draw_sprite()
 
         self.sprite.sprite_form = random.choice(forms)
         self.sprite.draw_sprite()
 
-    def quit_command(self):
-        myquit(self.master)
+        self.move_mouse_to_start_position()
+        self.generate_bind_mouse_event()
+
+
+    def stop_game(self, event=None):
+        self.canvas.delete("all")
+        self.canvas.configure(bg="white")
+        self.unbind_mouse_move()
+
+    def quit_command(self, event=None   ):
+        self.master.destroy()
+        sys.exit(0)
 
     def place_control(self, index, widget):
        widget.place(x=10 + index * 80, y=self.canvas_height + 1, width=70, height=self.bottom_command_line_height - 2)
@@ -296,9 +392,11 @@ class TApplication(tk.Frame):
         self.place_control(1, tk.Button(self, text="Новая", command=self.init_new_game))
         self.place_control(2, tk.Entry(textvariable=self.goals_count_str))
         self.place_control(3, tk.Radiobutton(text='основ', variable=self.collection_id, value=TCollection.basic))
-        self.place_control(4, tk.Radiobutton(text='машины', variable=self.collection_id, value=TCollection.vehicle))
+        self.place_control(4, tk.Radiobutton(text='машины', variable=self.collection_id, value=TCollection.vehicles))
         self.place_control(5, tk.Radiobutton(text='формы', variable=self.collection_id, value=TCollection.geometric))
-        self.place_control(6, tk.Radiobutton(text='все', variable=self.collection_id, value=TCollection.all))
+        self.place_control(6, tk.Radiobutton(text='буквы', variable=self.collection_id, value=TCollection.russian_abc))
+        self.place_control(7, tk.Radiobutton(text='птицы', variable=self.collection_id, value=TCollection.birds))
+        self.place_control(8, tk.Radiobutton(text='все', variable=self.collection_id, value=TCollection.all))
 
 
 def setup_logging(logfilename):
@@ -326,35 +424,20 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--not-fullscreen", dest='fullscreen', default=True, action="store_false")
     parser.add_argument("--goals-count", dest='goals_count', default=2, required=False)
-
     return parser.parse_args()
 
 
 def main(logger):
     args = parse_args()
     root = tk.Tk()
-    root.wm_protocol("WM_DELETE_WINDOW", partial(myquit, root) )
-    if args.fullscreen:
-        root.attributes('-fullscreen', True)
-    else:
-        root.geometry("600x500+100+100")
-    root.update()
-    tk_application = TApplication(logger, master=root, goals_count=args.goals_count)
-
+    tk_application = TApplication(args, logger, master=root)
     try:
         tk_application.mainloop()
     except Exception as e:
+        root.destroy()
         logger.error(e)
-        myquit()
 
 
 if __name__ == "__main__":
     logger = setup_logging("sorter.log")
     main(logger)
-
-
-#1. не работает
-#2. добавит картинки: домик, буквы, значки автомобиля, цифры
-#3. Добавить звук
-#4. Птицы  и их звучание
-#5. мышь
