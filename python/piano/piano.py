217a024a85fc72f93f8b0641b9d6e3522283c395
sokirko74 <alexey.sokirko@gmail.com>
2020-06-06 06:01:27 +0300

    use thread.join

--- python/piano/piano.py
+++ python/piano/piano.py
@@ -1,3 +1,6 @@
+from common.movements import detect_movements
+from common.bluetooth_koleso import TBluetoohKolesoThread
+
 import tkinter as tk
 import os
 from pathlib import Path
@@ -7,32 +10,27 @@ from pygame import mixer
 import logging
 import sys
 
-sys.path.append('..')
-from common.movements import detect_movements
-from common.bluetooth_koleso import  TBluetoohKolesoThread
 
-TK_APPLICATION = None
-full_path = os.path.realpath(__file__)
-FILEPATH, _ = os.path.split(full_path)
-SLIDE_SWITCH = os.path.join(FILEPATH, "sound/slide_switch.wav")
-BLUETOOTH_KOLESO = None
 ZYNADDSUBFX_BINARY = "zynaddsubfx"
 
+
 def setup_logging():
-    root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
+    logger = logging.getLogger("piano_logger")
+    logger.setLevel(logging.DEBUG)
 
     # create formatter and add it to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh = logging.FileHandler("piano.log",  mode='a')
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(formatter)
-    root.addHandler(fh)
+    logger.addHandler(fh)
 
     # create console handler with a higher log level
     ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    root.addHandler(ch)
+    ch.setLevel(logging.INFO)
+    logger.addHandler(ch)
+
+    return logger
 
 
 def current_iso8601():
@@ -41,49 +39,31 @@ def current_iso8601():
     # https://xkcd.com/1179/
     return time.strftime("%Y%m%dT%H%M%SZ", time.gmtime())
 
-def motion_handler_wrapper(event):
-    global TK_APPLICATION
-    TK_APPLICATION.motion_handler(event)
 
 def play_file(filename):
     mixer.init()
     mixer.music.load(filename)
     mixer.music.play()
 
-class Text2(tk.Frame):
-    def __init__(self, master, width=0, height=0, **kwargs):
-        self.width = width
-        self.height = height
 
-        tk.Frame.__init__(self, master, width=self.width, height=self.height)
-        self.text_widget = tk.Text(self, **kwargs)
-        self.text_widget.pack(expand=tk.YES, fill=tk.BOTH)
-
-    def pack(self, *args, **kwargs):
-        tk.Frame.pack(self, *args, **kwargs)
-        self.pack_propagate(False)
-
-    def grid(self, *args, **kwargs):
-        tk.Frame.grid(self, *args, **kwargs)
-        self.grid_propagate(False)
-
-
-class Application(tk.Frame):
+class TApplication(tk.Frame):
     def load_collection(self):
         collection = self.all_collections[self.collection_id]
-        folder = os.path.join(args.collection_folder, collection)
+        folder = os.path.join(self.args.collection_folder, collection)
         self.Instruments = [f for f in Path(folder).rglob('*.xiz')]
         print("use {} instruments\n".format(len(self.Instruments)))
 
-    def __init__(self, args, master=None):
+    def __init__(self, args, logger, master):
         self.args = args
-        self.Master = master
-        self.Master.bind('<Shift-n>', self.next_instrument)
-        self.Master.bind('<Shift-N>', self.next_instrument)
-        self.Master.bind("<Return>", self.on_enter_key)
+        self.master = master
+        self.logger = logger
+        self.collection_switcher_thread = None
+        self.master.bind('<Shift-n>', self.next_instrument)
+        self.master.bind('<Shift-N>', self.next_instrument)
+        self.master.bind("<Return>", self.on_enter_key)
 
-        master.geometry("900x600")
-        root.geometry("+400+600")
+        self.master.geometry("900x600")
+        self.master.geometry("+400+600")
 
         self.Instruments = None
         self.all_collections = os.listdir(args.collection_folder)
@@ -91,33 +71,58 @@ class Application(tk.Frame):
         self.load_collection()
 
         self.InstrumentIndex = 0
-        #self.CoordWidget = None
+        self.CollectionWidget = None
         self.InstrumentWidget = None
         self.InstrumentIndexWidget = None
         self.Points = list()
-        self.createWidgets()
+        self.create_widgets()
         self.run_zynadd()
         self.bind_moise_move()
+        self.master.wm_protocol("WM_DELETE_WINDOW", self.quit)
+        self.init_collection_switcher()
 
-    def print_log(self, widget, s):
+    def init_collection_switcher(self):
+        self.collection_switcher_thread = None
+        try:
+            self.collection_switcher_thread = TBluetoohKolesoThread(self.logger, self.switch_instrument)
+        except Exception as e:
+            self.logger.info(
+                "cannot find bluetooth koleso, ignore it: {}, try to run 'sudo /etc/init.d/bluetooth restart' ".format(
+                    e))
+
+    def start_collection_switcher(self):
+        if self.collection_switcher_thread is not None:
+            self.collection_switcher_thread.start()
+
+    def stop_collection_switcher(self):
+        if self.collection_switcher_thread is not None:
+            self.collection_switcher_thread.killed = True
+            time.sleep(1)
+            self.collection_switcher_thread.join(1)
+            del self.collection_switcher_thread
+            self.collection_switcher_thread = None
+
+    def print_to_widget(self, widget, s):
         widget.delete('1.0', tk.END)
         widget.insert(tk.END, s)
 
-    def createWidgets(self):
+    def create_widgets(self):
         self.now = tk.StringVar()
-        tk.Frame.__init__(self, self.Master)
+        tk.Frame.__init__(self, self.master)
         self.pack()
         self.time = tk.Label(self, font=('Helvetica', 24))
         self.time.pack(side="top")
         self.time["textvariable"] = self.now
 
-        tk.Button(master=self, text='Exit', command=myquit).pack(side=tk.TOP)
-
+        tk.Button(master=self, text='Exit', command=self.quit).pack(side=tk.TOP)
+        self.CollectionWidget = tk.Text(self, width=80, height=1, font=("Helvetica", 20))
+        self.CollectionWidget.pack(side=tk.TOP)
         self.InstrumentWidget = tk.Text(self, width=80, height=1, font=("Helvetica", 20))
         self.InstrumentWidget.pack(side=tk.TOP)
 
-        self.CoordWidget = tk.Text(self, width=900, height=4)
-        self.CoordWidget.pack(side=tk.TOP)
+        if self.args.enable_mouse_instrument_switch:
+            self.CoordWidget = tk.Text(self, width=900, height=4)
+            self.CoordWidget.pack(side=tk.TOP)
 
         self.InstrumentIndexWidget = tk.Text(self, width=3, height=1, font=("Helvetica", 48))
         self.InstrumentIndexWidget.pack(side=tk.TOP)
@@ -128,7 +133,7 @@ class Application(tk.Frame):
         self.Canvas.pack(fill=tk.BOTH, expand=1)
 
         # initial time display
-        self.onUpdate()
+        self.on_update()
 
     def clear_canvas(self):
         self.Points = list()
@@ -140,12 +145,12 @@ class Application(tk.Frame):
             self.Canvas.create_line(self.Points[i-1][1], self.Points[i-1][2], self.Points[i][1], self.Points[i][2])
 
     def bind_moise_move(self):
-        if args.enable_mouse_instrument_switch:
-            self.Master.bind('<Motion>', motion_handler_wrapper)
+        if self.args.enable_mouse_instrument_switch:
+            self.master.bind('<Motion>', self.motion_handler)
 
     def unbind_moise_move(self):
-        if args.enable_mouse_instrument_switch:
-            self.Master.unbind('<Motion>')
+        if self.args.enable_mouse_instrument_switch:
+            self.master.unbind('<Motion>')
 
     def on_enter_key(self, event):
         newIndex = int(self.InstrumentIndexWidget.get("1.0", tk.END).strip())
@@ -198,11 +203,11 @@ class Application(tk.Frame):
             self.switch_instrument(movement)
         self.clear_canvas()
 
-    def onUpdate(self):
+    def on_update(self):
         # update displayed time
         self.now.set(current_iso8601())
         curtime = time.time()
-        if args.enable_mouse_instrument_switch:
+        if self.args.enable_mouse_instrument_switch:
             if self.focus_get() != None and len(self.Points) > 0:
                 last_move_time = self.Points[-1][0]
                 if curtime - last_move_time > 0.5:
@@ -213,16 +218,17 @@ class Application(tk.Frame):
                     self.bind_moise_move()
 
         # schedule timer to call myself after 1 second
-        self.after(200, self.onUpdate)
-
+        self.after(200, self.on_update)
 
     def run_zynadd(self):
         instrument = self.Instruments[self.InstrumentIndex]
-        self.print_log(self.InstrumentWidget, "{}".format(instrument))
-        self.print_log(self.InstrumentIndexWidget, "{}".format(self.InstrumentIndex))
+        self.print_to_widget(self.CollectionWidget, "{}".format(self.all_collections[self.collection_id]))
+        self.print_to_widget(self.InstrumentWidget, "{}".format(os.path.basename(instrument)))
+        self.print_to_widget(self.InstrumentIndexWidget, "{}".format(self.InstrumentIndex))
 
         os.system ("nohup pkill {0} >>binary_spawn_log.txt 2>&1 ".format(ZYNADDSUBFX_BINARY))
-        play_file(SLIDE_SWITCH)
+        slide_switch = os.path.join(os.path.dirname(os.path.realpath(__file__)), "sound", "slide_switch.wav")
+        play_file(slide_switch)
 
         cmd = "{} --auto-connect --output {} --load-instrument='{}' ".format(ZYNADDSUBFX_BINARY, self.args.sound_server, instrument)
         if not self.args.use_gui:
@@ -231,7 +237,6 @@ class Application(tk.Frame):
         os.system(cmd)
 
         logging.debug('use instrument {}'.format(instrument))
-
 
     def motion_handler(self, event):
         cur_x = event.x
@@ -243,7 +248,15 @@ class Application(tk.Frame):
         self.Points.append(point)
         self.Points = [(t, x, y) for (t, x, y) in self.Points if curtime - t <= 2.0]
         self.draw_points()
-        self.print_log(self.CoordWidget, '{}'.format(str([(x,y) for (_, x, y) in self.Points])))
+        if self.CoordWidget is not None:
+            self.print_to_widget(self.CoordWidget, '{}'.format(str([(x, y) for (_, x, y) in self.Points])))
+
+    def quit(self):
+        self.stop_collection_switcher()
+        os.system("pkill ffplay")
+        os.system("pkill {0}".format(ZYNADDSUBFX_BINARY))
+        self.master.destroy()
+        sys.exit(0)
 
 
 def parse_args():
@@ -259,35 +272,24 @@ def parse_args():
     return parser.parse_args()
 
 
-def bluetooth_koleso_action(is_forward):
-    global TK_APPLICATION
-    TK_APPLICATION.switch_instrument("up" if is_forward else "down")
 
-def myquit():
-    global BLUETOOTH_KOLESO
-    if BLUETOOTH_KOLESO is not None:
-        BLUETOOTH_KOLESO.killed = True
-    os.system("pkill ffplay")
-    os.system("pkill {0}".format(ZYNADDSUBFX_BINARY))
-    tk.Tk().destroy()
-    sys.exit(0)
+
+def main():
+    args = parse_args()
+    logger = setup_logging()
+    tk_root = tk.Tk()
+    if args.fullscreen:
+        tk_root.attributes('-fullscreen', True)
+    tk_app = TApplication(args, logger, tk_root)
+
+    tk_app.start_collection_switcher()
+    play_file("sound/slide_switch.wav")
+
+    try:
+        tk_app.mainloop()
+    finally:
+        tk_app.stop_collection_switcher()
+
 
 if __name__ == '__main__':
-    args = parse_args()
-    root = tk.Tk()
-    root.wm_protocol("WM_DELETE_WINDOW", myquit)
-    setup_logging()
-    try:
-        BLUETOOTH_KOLESO = TBluetoohKolesoThread(bluetooth_koleso_action)
-    except Exception as e:
-        logging.info("cannot find bluetooth koleso, ignore it: {}, try to run 'sudo /etc/init.d/bluetooth restart' ".format(e))
-
-    play_file("sound/slide_switch.wav")
-    if args.fullscreen:
-        root.attributes('-fullscreen', True)
-    TK_APPLICATION = Application(args, master=root)
-    if BLUETOOTH_KOLESO is not None:
-        BLUETOOTH_KOLESO.start()
-
-    root.mainloop()
-
+    main()
