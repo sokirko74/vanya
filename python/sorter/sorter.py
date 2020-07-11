b5614dd668daf1ff8ba1d8c595fddf19266e3ee8
Alexey Sokirko <sokirko@yandex.ru>
2020-07-11 08:56:47 +0300

    use more classes

--- python/sorter/sorter.py
+++ python/sorter/sorter.py
@@ -165,12 +165,40 @@ class TSprite:
         return None
 
 
-def read_folder(collection_id):
-    sprites = list()
-    folder = TCollection.get_collection_name(collection_id)
-    for fpath in os.listdir(os.path.join(os.path.dirname(__file__), folder)):
-        sprites.append(TSpriteForm(collection_id, image_path=os.path.join(folder, fpath)) )
-    return sprites
+class TSpriteFormCollections:
+
+    @staticmethod
+    def _read_folder(collection_id):
+        sprites = list()
+        folder = TCollection.get_collection_name(collection_id)
+        for fpath in os.listdir(os.path.join(os.path.dirname(__file__), folder)):
+            sprites.append(TSpriteForm(collection_id, image_path=os.path.join(folder, fpath)))
+        return sprites
+
+    def __init__(self):
+        self.basic_forms = TSpriteFormCollections._read_folder(TCollection.basic)
+        self.vehicles_forms = TSpriteFormCollections._read_folder(TCollection.vehicles)
+        self.geometric_forms = [TSpriteForm(TCollection.geometric, form=f, color=c) for c in
+                                ["blue", "red", "yellow", "black"] \
+                                for f in ["rectangle", "triangle", "circle", "pentagram"]]
+        self.russian_abc = [TSpriteForm(TCollection.russian_abc, char=a) for a in
+                            "АЕИЯЮЭЁАЕИЯЮЭЁАБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЭЮЯ"]
+        # three times vowels
+        self.bird = TSpriteFormCollections._read_folder(TCollection.birds)
+
+    def get_forms(self, c_id):
+        if c_id == TCollection.basic:
+            return self.basic_forms
+        elif c_id == TCollection.vehicles:
+            return self.vehicles_forms
+        elif c_id == TCollection.geometric:
+            return self.geometric_forms
+        elif c_id == TCollection.russian_abc:
+            return self.russian_abc
+        elif c_id == TCollection.birds:
+            return self.bird
+        else:
+            return self.basic_forms + self.vehicles_forms + self.geometric_forms + self.russian_abc + self.bird
 
 
 class TApplication(tk.Frame):
@@ -184,42 +212,36 @@ class TApplication(tk.Frame):
         self.set_main_window_geometry()
         self.pack(expand=True, fill=tk.BOTH)
         self.canvas = tk.Canvas(self, bg="white")
-        self.goal_up_margin = 10
-        self.goal_horizontal_margin = 10
         self.collection_id = tk.IntVar(value=TCollection.basic)
         self.bottom_command_line_height = 20
         self.canvas_height = self.master.winfo_height() - self.bottom_command_line_height
-        self.basic_forms = read_folder(TCollection.basic)
-        self.vehicles_forms = read_folder(TCollection.vehicles)
-        self.geometric_forms = [TSpriteForm(TCollection.geometric, form=f, color=c) for c in ["blue", "red", "yellow", "black"] \
-                                    for f in ["rectangle", "triangle", "circle", "pentagram"] ]
-        self.russian_abc = [TSpriteForm(TCollection.russian_abc, char=a) for a in "АЕИЯЮЭЁАЕИЯЮЭЁАБВГДЕЁЖЗИКЛМНОПРСТУФХЦЧШЩЭЮЯ"]
-                                                        # three times vowels
-        self.bird = read_folder(TCollection.birds)
+        self.collections = TSpriteFormCollections()
         self.goals_count_str = tk.StringVar()
         self.goals_count_str.set(str(self.goals_count))
         self.goals = None
         self.sprite = TSprite(self)
         self.create_widgets()
-        master.bind('<Left>', self.left_key)
-        master.bind('<Right>', self.right_key)
-        master.bind('<Up>', self.up_key)
-        master.bind('<Down>', self.down_key)
-        master.bind("<Configure>", self.resize)
+        self.bind_keys()
+        self.moving_step = 5
+        self.orientation = 'bottom_up'
+        master.wm_protocol("WM_DELETE_WINDOW", self.quit_command )
+        self.canvas.update()
+
+    def bind_keys(self):
+        self.master.bind('<Left>', self.left_key)
+        self.master.bind('<Right>', self.right_key)
+        self.master.bind('<Up>', self.up_key)
+        self.master.bind('<Down>', self.down_key)
+        self.master.bind("<Configure>", self.resize)
         self.master.bind('<Control-n>', self.init_new_game)
         self.master.bind('<Control-N>', self.init_new_game)
-        self.master.bind("<Button-1>", self.init_new_game)
+        self.master.bind("<Button-3>", self.init_new_game)
         self.master.bind('<Control-x>', self.quit_command)
         self.master.bind('<Control-X>', self.quit_command)
         self.master.bind('<Control-c>', self.stop_game)
         self.master.bind('<Control-C>', self.stop_game)
         self.master.bind('<Control-b>', self.bind_mouse)
-        self.moving_step = 5
-        master.wm_protocol("WM_DELETE_WINDOW", self.quit_command )
-        self.canvas.update()
-        start_x = (self.canvas.winfo_width() - TSpriteForm.sprite_width) / 2
-        start_y = self.canvas.winfo_height() - TSpriteForm.sprite_height - 2
-        self.start_point = TPoint(start_x, start_y)
+        self.unbind_mouse_move()
 
     def set_main_window_geometry(self):
         if self.args.fullscreen:
@@ -244,7 +266,6 @@ class TApplication(tk.Frame):
             m = self.sprite.matches(g)
             if m is not None:
                 self.unbind_mouse_move()
-                self.move_mouse_to_start_position()
                 if m:
                     self.canvas.configure(bg="green")
                     self.canvas.update()
@@ -253,8 +274,6 @@ class TApplication(tk.Frame):
                     self.canvas.configure(bg="red")
                     self.canvas.update()
                     play_file("fail.mp3")
-                #self.init_new_game()
-                return
 
     def redraw_canvas(self, sprite_x, sprite_y):
         if self.sprite.point is None:
@@ -327,52 +346,76 @@ class TApplication(tk.Frame):
     def unbind_mouse_move(self):
         self.master.unbind('<Motion>')
 
-    def move_mouse_to_start_position(self):
-        #sys.stderr.write("move_mouse_to_start_position\n".format())
-        self.move_mouse(self.start_point.x, self.start_point.y)
+    def get_start_point(self, padding=10):
+        vertical_half = (self.canvas.winfo_width() - TSpriteForm.sprite_width) / 2
+        horizontal_half = (self.canvas.winfo_height() - TSpriteForm.sprite_height) / 2
+        bottom = self.canvas.winfo_height() - TSpriteForm.sprite_height - padding
+        up = padding
+        left = padding
+        right = self.canvas.winfo_width() - TSpriteForm.sprite_width - padding
 
-    def get_forms(self):
-        c_id = self.collection_id.get()
-        if c_id == TCollection.basic:
-            return self.basic_forms
-        elif c_id == TCollection.vehicles:
-            return self.vehicles_forms
-        elif c_id == TCollection.geometric:
-            return self.geometric_forms
-        elif c_id == TCollection.russian_abc:
-            return self.russian_abc
-        elif c_id == TCollection.birds:
-            return self.bird
-        else:
-            return self.basic_forms + self.vehicles_forms + self.geometric_forms + self.russian_abc + self.bird
+        if self.orientation == 'bottom_up':
+            return TPoint(vertical_half, bottom)
+        if self.orientation == 'up_bottom':
+            return TPoint(vertical_half, up)
+        if self.orientation == 'left_right':
+            return TPoint(left, horizontal_half)
+        if self.orientation == 'right_left':
+            return TPoint(right, horizontal_half)
+        assert False
 
+    def draw_goals_horizontal(self, forms, padding, y):
+        sum_goals_width = TSpriteForm.sprite_width * self.goals_count
+        gap_width = (self.canvas.winfo_width() - 2 * padding - sum_goals_width) / (self.goals_count - 1)
+        x = padding
+
+        for f, g in zip(forms, self.goals):
+            g.sprite_form = f
+            g.point = TPoint(x, y)
+            x += TSpriteForm.sprite_width + gap_width
+            g.draw_sprite()
+
+    def draw_goals_vertically(self, forms, padding, x):
+        sum_goals_height = TSpriteForm.sprite_height * self.goals_count
+        gap_height = (self.canvas.winfo_height() - 2 * padding - sum_goals_height) / (self.goals_count - 1)
+        y = padding
+
+        for f, g in zip(forms, self.goals):
+            g.sprite_form = f
+            g.point = TPoint(x, y)
+            y += TSpriteForm.sprite_height + gap_height
+            g.draw_sprite()
 
     def init_new_game(self, event=None):
+        padding = 10
         self.canvas.delete("all")
         self.canvas.configure(bg="white")
         self.goals_count = int(self.goals_count_str.get())
         self.goals = [TSprite(self) for i in range(self.goals_count)]
+        self.orientation = random.choice(['bottom_up', 'up_bottom', 'left_right', 'right_left'])
+        #self.orientation = 'right_left'
+        start_point = self.get_start_point(padding)
+        self.sprite.point = TPoint(start_point.x, start_point.y)
 
-        self.sprite.point = TPoint(self.start_point.x, self.start_point.y)
-        all_forms = self.get_forms()
+        all_forms = self.collections.get_forms(self.collection_id.get())
         forms = random.sample(all_forms, self.goals_count)
         for f in forms:
             self.logger.debug(str(f))
-        sum_goals_width = TSpriteForm.sprite_width * self.goals_count
-        gap_width = (self.canvas.winfo_width() - 2 * self.goal_horizontal_margin - sum_goals_width)/(self.goals_count -1)
-        x = self.goal_horizontal_margin
-        for f, g in zip(forms, self.goals):
-            g.sprite_form = f
-            g.point = TPoint(x, self.goal_up_margin)
-            x += TSpriteForm.sprite_width + gap_width
-            g.draw_sprite()
+
+        if self.orientation == 'bottom_up':
+            self.draw_goals_horizontal(forms, padding, padding)
+        if self.orientation == 'up_bottom':
+            self.draw_goals_horizontal(forms, padding, self.canvas.winfo_height() - TSpriteForm.sprite_height - padding)
+        if self.orientation == 'left_right':
+            self.draw_goals_vertically(forms, padding, self.canvas.winfo_width() - TSpriteForm.sprite_width - padding)
+        if self.orientation == 'right_left':
+            self.draw_goals_vertically(forms, padding, padding)
 
         self.sprite.sprite_form = random.choice(forms)
         self.sprite.draw_sprite()
 
-        self.move_mouse_to_start_position()
+        self.move_mouse(start_point.x, start_point.y)
         self.generate_bind_mouse_event()
-
 
     def stop_game(self, event=None):
         self.canvas.delete("all")
