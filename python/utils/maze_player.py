ce1c561ada6ba8370fe03c09191550a97026a037
sokirko74 <sokirko@yandex.ru>
2022-01-25 18:20:17 +0300

    use shadow sprites

--- python/utils/maze_player.py
+++ python/utils/maze_player.py
@@ -2,7 +2,7 @@ import pygame
 import os
 import math
 from pygame.math import Vector2
-import random
+import threading
 import time
 
 
@@ -12,7 +12,7 @@ def unit_vector(angle, coef):
     return Vector2(round(coef * math.cos(theta)), round(coef * math.sin(theta)))
 
 
-class Player(pygame.sprite.Sprite):
+class MazePlayer(pygame.sprite.Sprite):
     def __init__(self, parent, image, height, width, max_speed, sound_moving,  size=2,
                  sound_crash=os.path.join('assets', 'sounds', 'bee_crash.wav'),
                  sound_success=os.path.join('assets', 'sounds', 'success.wav')):
@@ -20,7 +20,7 @@ class Player(pygame.sprite.Sprite):
         self.angle = 180
         self.parent = parent
         self.screen_rect = self.parent.maze_rect
-        self.image = pygame.image.load(image)
+        self.image = image
         self.orig_image = self.image.copy()
         self.sound_moving = sound_moving
         self.sound_crash = pygame.mixer.Sound(sound_crash)
@@ -28,12 +28,28 @@ class Player(pygame.sprite.Sprite):
         self.sound_success = pygame.mixer.Sound(sound_success)
         self.width = width
         self.height = height
+        self.music_thread = threading.Thread(target=self.watch_moving)
+        self.music_thread.daemon = True
+        self.run = True
+        self.last_move_time_stamp = None
+        self.music_thread.start()
         pygame.mixer.music.load(self.sound_moving)
-        pygame.mixer.music.play(-1, fade_ms=2000)
         pygame.mixer.music.set_volume(0.2)
+
+
         self.max_speed = max_speed
         self.score = 0
         self.set_scale(size)
+
+    def watch_moving(self):
+        while self.run:
+            if self.last_move_time_stamp is not None:
+                if time.time() - self.last_move_time_stamp > 1:
+                    pygame.mixer.music.stop()
+                elif not pygame.mixer.music.get_busy():
+                    print ("play music")
+                    pygame.mixer.music.play(loops=-1, fade_ms=2000)
+            time.sleep(0.5)
 
     def get_sound_success(self):
         return self.sound_success
@@ -57,7 +73,7 @@ class Player(pygame.sprite.Sprite):
                 self.parent.chan_2.play(self.get_sound_success())
             self.score += 1
             time.sleep(1)
-            pygame.mixer.music.play(-1, fade_ms=2000)
+            #pygame.mixer.music.play(-1, fade_ms=2000)
             self.kill()
             for f in self.parent.objects:
                 f.kill()
@@ -74,6 +90,7 @@ class Player(pygame.sprite.Sprite):
                 collided_objects = pygame.sprite.spritecollideany(self, self.parent.objects, collided=pygame.sprite.collide_mask)
                 if collided_objects is not None:
                     collided_objects.contact_object()
+                self.last_move_time_stamp = time.time()
                 return True
 
     def set_initial_position(self, pos):
@@ -84,6 +101,8 @@ class Player(pygame.sprite.Sprite):
     def handle_event(self, event):
         pass
 
-
+    def stop_playing(self):
+        self.run = False
+        self.music_thread.join(1)
 
 
