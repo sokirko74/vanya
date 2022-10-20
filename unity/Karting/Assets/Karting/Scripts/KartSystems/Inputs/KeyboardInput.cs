a4b238295d1e631a28823dbddc24b43d3a48f6c2
sokirko74 <alexey.sokirko@gmail.com>
2022-10-20 21:07:54 +0300

    use karting template

---
+++ unity/Karting/Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput : BaseInput
+    {
+        public string TurnInputName = "Horizontal";
+        public string AccelerateButtonName = "Accelerate";
+        public string BrakeButtonName = "Brake";
+        
+        public override InputData GenerateInput() {
+            /*var axis = new string[] { "Horizontal", "Vertical", "Accelerate", "Axis 1", "Axis 2", "Axis 3" };
+            string info = "";
+            foreach (string a in axis)
+            {
+
+                info += string.Format("{0}={1},", a, Input.GetAxis(a));
+            }
+            print(info);*/
+            bool accelerate = Input.GetButton(AccelerateButtonName);
+            if (Input.GetAxis("Accelerate") > 0)
+            {
+                accelerate = true;
+            }
+
+            return new InputData
+            {
+                //Accelerate = Input.GetAxis(AccelerateButtonName),
+                Accelerate = accelerate,
+                Brake = Input.GetButton(BrakeButtonName),
+                TurnInput = Input.GetAxis("Horizontal")
+                
+            };
+        }
+    }
+}
