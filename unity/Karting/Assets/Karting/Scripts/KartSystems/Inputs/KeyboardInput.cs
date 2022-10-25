57912cf4e52c14c45607ad5196a18ff07764940b
sokirko74 <alexey.sokirko@gmail.com>
2022-10-25 15:10:25 +0300

    joystick axis2 -> accelerate

--- unity/Karting/Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
+++ unity/Karting/Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -1,19 +1,55 @@
 ﻿using UnityEngine;
 
-namespace KartGame.KartSystems {
+namespace KartGame.KartSystems
+{
 
     public class KeyboardInput : BaseInput
     {
+        public static bool GlobalStarted = false;
         public string TurnInputName = "Horizontal";
         public string AccelerateButtonName = "Accelerate";
         public string BrakeButtonName = "Brake";
 
-        public override InputData GenerateInput() {
+        public override InputData GenerateInput()
+        {
+            bool accelerate = Input.GetButton(AccelerateButtonName);
+            float axis2 = Input.GetAxis("Axis 2");
+            if (!GlobalStarted)
+            {
+                if (axis2 != 0)
+                {
+                    GlobalStarted = true;
+                }
+                else
+                {
+                    axis2 = -1;
+                }
+            }
+            if (axis2 > -0.98)
+            {
+                accelerate = true;
+            }
+            //accelerate = false;
+            var axis = new string[] { "Horizontal", "Vertical", "Accelerate", "Axis 1", "Axis 2", "Axis 3" };
+            string info = "";
+            foreach (string a in axis)
+            {
+
+                info += string.Format("{0}={1},", a, Input.GetAxis(a));
+            }
+
+            info += string.Format("=>pedal={0},", accelerate);
+            info += string.Format(",GlobalStarted={0},", GlobalStarted);
+
+            print(info);
+
             return new InputData
             {
-                Accelerate = Input.GetButton(AccelerateButtonName),
-                Brake = Input.GetButton(BrakeButtonName),
+                Accelerate = accelerate,
+                //Brake = Input.GetButton(BrakeButtonName),
+                Brake = false,
                 TurnInput = Input.GetAxis("Horizontal")
+
             };
         }
     }
