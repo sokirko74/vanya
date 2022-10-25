99c5e2b7958b0b01969ed23e99cd1b408d1da492
sokirko74 <alexey.sokirko@gmail.com>
2022-10-25 11:52:17 +0300

    clean fork

--- unity/Karting/Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
+++ unity/Karting/Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -7,33 +7,13 @@ namespace KartGame.KartSystems {
         public string TurnInputName = "Horizontal";
         public string AccelerateButtonName = "Accelerate";
         public string BrakeButtonName = "Brake";
-        
+
         public override InputData GenerateInput() {
-            bool accelerate = Input.GetButton(AccelerateButtonName);
-            float vertical = Input.GetAxis("Vertical");
-            if (vertical > -0.98 && vertical != 0)
-            {
-                accelerate = true;
-            }
-            //accelerate = false;
-            var axis = new string[] { "Horizontal", "Vertical", "Accelerate", "Axis 1", "Axis 2", "Axis 3" };
-            string info = "";
-            foreach (string a in axis)
-            {
-
-                info += string.Format("{0}={1},", a, Input.GetAxis(a));
-            }
-
-            info += string.Format("=>pedal={0},", accelerate);
-            print(info);
-
             return new InputData
             {
-                Accelerate = accelerate,
-                //Brake = Input.GetButton(BrakeButtonName),
-                Brake = false,
+                Accelerate = Input.GetButton(AccelerateButtonName),
+                Brake = Input.GetButton(BrakeButtonName),
                 TurnInput = Input.GetAxis("Horizontal")
-               
             };
         }
     }
