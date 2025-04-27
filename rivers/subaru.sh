61a62267e9bdb384798351f1078c651e3fa335cd
sokirko14 <ivan.a.sokirko@gmail.com>
2025-04-27 08:35:19 +0300

    add vv alarm

---
+++ rivers/subaru.sh
@@ -0,0 +1,3 @@
+cd  ~/vanya/python/rivers;
+python3 rivers.py --width 1900 --height 1000 --car-sprite-folder assets/sprites/gruzovik  --bridge-width 900 \
+--max-car-speed-limit 4 --engine-audio-folder assets/sounds/subaru --angle-level-ratio 30
